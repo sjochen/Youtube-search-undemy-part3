0f570d698938142827ab1f221f7327cb8a6ebef5
Stephen Jochen <stephenjochen2@gmail.com>
2020-01-16 12:13:21 -0500

    added the YouTube API and logged the results.

--- assets/script.js
+++ assets/script.js
@@ -24,4 +24,40 @@ $(function(){
         }, 400)
        }
     });
+
+    $('#search-form').submit(function(e){
+        e.preventDefault();
+    })
 })
+
+function search() {
+    //Clear Results
+    $('#results').html('');
+    $('#buttons').html('');
+
+    //Get Form Input
+    q = $('#query').val();
+
+    //Run GET Request on API
+    $.get(
+        "https://www.googleapis.com/youtube/v3/search",{
+            part: 'snippet, id',
+            q: q,
+            type:'video',
+            key: 'AIzaSyAzZWr7dqf6HAMoZItSvMGNG38v8A3C5oM'},
+            function (data) {
+                let nextPageToken = data.nextPageToken;
+                let previousPageToken = data.previousPageToken;
+
+                console.log(data);
+
+                $.each(data.items, function (i, item) {
+                    let output = getOutput(item);
+
+                    //Display Results
+                    $('#results').append(output);
+                })
+
+            }
+    );
+}
