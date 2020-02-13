18921d53b758a7f8e0ed23167036953fc2c33794
Stephen Jochen <stephenjochen2@gmail.com>
2020-02-12 19:30:23 -0500

    added fancybox, need to trouble shoot googles samesite cookie update for fancy box to run properly

--- assets/script.js
+++ assets/script.js
@@ -163,7 +163,7 @@ function getOutput(item) {
     '<img src="'+ thumb +'">' +
     '</div>' +
     '<div class="list-right">' +
-    '<h3>' + title + '<h3>' +
+    '<h3><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/' + videoId +'">' + title + '</a><h3>' +
     '<small>By <span class="cTitle">' + channelTitle + '</span> on ' + videoDate + 
     '<p>' + description + '<p> </small>' +
     '</div>' +
